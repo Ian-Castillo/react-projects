32b5563f34a408b216ba02b633765789dd790597
Ian Castillo <i.michaelcastillo@gmail.com>
2021-04-01 14:22:10 -0500

    First Pass Done

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,45 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const conatiner = useRef(null);
+  const[columnes,SetColumes] = useState('col-2')
+  useEffect(() => {
+    SetColumes('col-2')
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${center}px`;
 
-export default Submenu
+  if (links.length === 3) {
+  setColumns('col-3')
+  }
+  if(links.length > 3){
+  setColumns('col-4')
+}
+}, [location,links])
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columes}`}>
+        {links.map((link,index) => {
+          const {label,icon,url} = linkreturn <a key={index} href = {url}>
+          {icon}
+          {label}
+</a>
+        ) 
+      })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
